8baf7e145426a1f9755ccc44d3144480949ef85a
Teubel György <tgyurci@gmail.com>
2016-02-02 11:03:07 +0100

    First step to default UTF-8 on BSDs, xclude temp files from sync

--- sync.sh
+++ sync.sh
@@ -7,4 +7,4 @@ else
 	dry_run=""
 fi
 
-exec rsync -rtmv $dry_run --exclude .ssh --exclude .local.sh --exclude .local.bash --exclude .local.zsh rc/ $1
+exec rsync -rtmv $dry_run --exclude '*.swp' --exclude .ssh --exclude .local.sh --exclude .local.bash --exclude .local.zsh rc/ "$1"
